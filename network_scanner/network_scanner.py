14bd03040f9b8a4574de0db69eb1076f885f714c
infectsoldier <777rip777@gmail.com>
2018-08-27 17:40:10 +0300

    added travis file

--- network_scanner/network_scanner.py
+++ network_scanner/network_scanner.py
@@ -4,7 +4,8 @@ import argparse
 
 def get_arguments():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-t", "--target", dest="target", help="Target IP/IP Range")
+    parser.add_argument("-t", "--target", dest="target",
+                        help="Target IP/IP Range")
     options = parser.parse_args()
     return options
 
@@ -13,7 +14,8 @@ def scan(ip):
     arp_request = scapy.ARP(pdst=ip)
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
     arp_request_broadcast = broadcast/arp_request
-    answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
+    answered_list = scapy.srp(arp_request_broadcast, timeout=1,
+                              verbose=False)[0]
 
     clients_list = []
     for element in answered_list:
